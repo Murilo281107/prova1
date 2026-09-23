d7cb6f9ac64436c23aee43bd29d88b256d22b600
Murilo281107 <murilobd2007@gmail.com>
2026-09-23 12:17:27 -0300

    finalizada

---
+++ candidatosTSE/src/main/java/com/example/CandidatosTSE/application/CandidatosTSE.java
@@ -0,0 +1,13 @@
+package com.example.CandidatosTSE.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatosTSE {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatosTSE.class, args);
+	}
+
+}
---
+++ candidatosTSE/src/test/java/com/example/CandidatosTSE/application/CandidatosTSETests.java
@@ -0,0 +1,13 @@
+package com.example.CandidatosTSE.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatosTSETests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
